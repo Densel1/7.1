5a538ae7166570def27ffc722d433f3664ed3988
Igor Kastyrkin <kastyrkinIV@ya.ru>
2023-09-18 13:36:57 +0300

    initial commit

---
+++ 1/1/1/1.cpp
@@ -0,0 +1,76 @@
+﻿// 1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "timer.h"
+#include <iostream>
+#include <vector>
+#include <random>
+#include <algorithm>
+#include <execution>
+#include <chrono>
+#include <thread>
+#include <windows.h>
+
+using namespace std;
+
+void set_cursor(int x = 0, int y = 0)
+{
+    HANDLE handle;
+    COORD coordinates;
+    handle = GetStdHandle(STD_OUTPUT_HANDLE);
+    coordinates.X = x;
+    coordinates.Y = y;
+    SetConsoleCursorPosition(handle, coordinates);
+}
+
+void virtual_client(int& q)
+{
+    for (int i = 0; i < 10; i++)
+    {
+        std::this_thread::sleep_for(1s);
+        q++;
+        set_cursor(14, 1);
+        cout << " " << q << " ";
+    }
+}
+
+void virtual_emploe(int& q)
+{
+    for (int i = 0; i < 10; i++) 
+    {
+        this_thread::sleep_for(2s);
+        q--;
+        set_cursor(14, 1);
+        cout << " " << q << " ";
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    int que = 0;
+
+    std::cout <<  "Количество аппаратных ядер - " << std::thread::hardware_concurrency() << std::endl;
+    
+    cout << "Длина очереди:";
+    thread t_c(virtual_client, ref(que));
+    thread t_e(virtual_emploe, ref(que));
+    t_c.join();
+    t_e.join();
+
+
+
+    return 0;
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
---
+++ 1/2/2/2.cpp
@@ -0,0 +1,73 @@
+﻿
+#include "timer.h"
+#include <iostream>
+#include <vector>
+#include <random>
+#include <thread>
+#include <chrono>
+#include <iomanip>
+
+using namespace std;
+
+float sum(vector<int> &v1, vector<int>& v2, int startElement, int endElement)
+{
+    vector<int> v3(4'000'000);
+    auto mstime = chrono::high_resolution_clock::now();
+    for (auto it = startElement; it != endElement; ++it)
+    {
+        v3.at(it) = v1.at(it) + v2.at(it);
+    }
+    auto metime = chrono::high_resolution_clock::now();
+    chrono::duration<double, milli> rtime = metime - mstime;
+    return rtime.count();
+}
+
+
+int main()
+{
+    const int thousend = 1000;
+    const int tenT = thousend * 10;
+    const int hundredT = thousend * 100;
+    const int thousendT = thousend * 1000;
+
+    vector<int> V1(thousendT*2);
+    vector<int> V2(thousendT*2);
+    mt19937 gen;
+    uniform_int_distribution<int> dis(0, thousendT);
+    auto rand_num([=]() mutable {return dis(gen); });
+    generate(V1.begin(), V1.end(), rand_num);
+    generate(V2.begin(), V2.end(), rand_num);
+
+
+
+    for (int j = thousend; j <= thousend * 1000; j *= 10) { cout << "\t\t" << j; }
+    cout << endl;
+
+     for (int k = 1; k <= 16; k *= 2)
+    {
+        vector<thread> VT3;
+
+        cout << endl << k << " potoka:";
+        for (int i = thousend; i <= thousend * 1000; i *= 10)
+        {
+            float sum1 = 0.0;
+            for (int j = 0; j < k; ++j)
+            {
+
+                if (j * (i / k) >= i) cout << "big_first_number" << endl;
+                if ((j * (i / k) + i / k) > i) cout << "big_second_number" << endl;
+
+                VT3.push_back(thread([&]() {sum1 += sum(ref(V1), ref(V2), j * (i / k), j * (i / k) + i / k); }));
+            }
+            this_thread::sleep_for(150ms); // please increase if your PC to slow
+            cout << '\t' << fixed << setprecision(4) <<sum1 / k << 's' << '\t';
+
+        }
+        for (auto& t1 : VT3)
+        {
+            if (t1.joinable()) t1.join();
+        }
+    }
+
+}
+
